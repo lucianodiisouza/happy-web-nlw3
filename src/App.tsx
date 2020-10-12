67f6d298ebecc52464430882dc61097cce65c844
lucianodiisouza <lucianodiisouza@hotmail.com>
2020-10-12 20:57:13 -0300

    component example

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 
+interface TitleProps {
+  text: string;
+}
+
+function Title(props: TitleProps) {
+  return <h1>{props.text}</h1>;
+}
+
 function App() {
   return (
     <div className="App">
-      <h1>Tudo pronto!</h1>
+      <Title text="Tudo mais ou menos" />
+      <Title text="Tudo quase pronto" />
+      <Title text="Tudo indo ficar pronto" />
+      <Title text="Tudo começando" />
     </div>
   );
 }
