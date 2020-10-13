1bbe3719c9b96ce43a2780319ccb4d9534f98b91
lucianodiisouza <lucianodiisouza@hotmail.com>
2020-10-12 22:00:48 -0300

    add: leaflet wip: map page

--- src/pages/Places.tsx
+++ src/pages/Places.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import { FiPlus } from "react-icons/fi";
+import mapMarker from "../images/map-marker.svg";
+
+import "../styles/pages/places.css";
 
 function Places() {
-  return <h1>Mapa</h1>;
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarker} alt="Happy" />
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Ribeirão das Neves</strong>
+          <span>Minas Gerais</span>
+        </footer>
+      </aside>
+
+      <div></div>
+
+      <Link to="" className="create-place">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+    </div>
+  );
 }
 
 export default Places;
